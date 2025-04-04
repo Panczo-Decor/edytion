1be52cdaef366a5946e232bef4d7dec78ad42598
Panczo-Decor <firanypanczo@gmail.com>
2025-04-04 20:55:57 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: 20 }}>
+      <h1>Asystent Gmail – Start!</h1>
+      <p>Witaj! Aplikacja działa poprawnie 🎉</p>
+    </div>
+  )
+}
+
+export default App
